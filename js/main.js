2a5870dd99d75ee70892651e89df382125a1a2bf
shaolin-wu <15265249516@163.com>
2018-02-03 20:57:37 +0800

    What: V1.2 使用marked.js 使markdown格式的代码转成html 修复一些bug
    Why:
    
    * html易于理解
    
    How:
    
    * 去marked的仓库学习使用

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-// // The code snippet you want to highlight, as a string
-// var text = "body{color: green};";
 
-// // Returns a highlighted HTML string
-// var css = Prism.highlight(text, Prism.languages.css);
-
-// console.log(css)
 var result = `/*
  *面试官你好，我是吴少林
  *我将以动画的形式来介绍我自己
@@ -43,14 +37,93 @@ html {
 
 /*好了， 我们不玩啦 */
 /* 接下来，我们准备一张白纸介绍一下自己吧*/
+#code {
+    position: fixed;
+    left: 0;
+    width: 50%;
+    height: 100%;
+}
+#paper{
+    position: fixed;
+    right: 0;
+    width: 50%;
+    height: 100%;
+    background: gray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+}
+#paper > .content{
+    background: white;
+    height: 100%;
+    width: 100%;
+
+}
 `
 var result2 = `
 #paper{
-    width: 100px; 
-    height: 100px;
-    background: red;
+    
 }
 `
+var md = `
+# 自我介绍
+
+我叫 吴少林
+1991年 11月 7日出生
+山东科技大学毕业
+自学编程2年
+自学前端半年
+
+# 技能介绍
+
+熟悉JavaScript HTML CSS3 JQuery
+
+# 项目介绍
+
+1. 各种轮播
+[苹果风格的轮播](https://github.com/codevvvv9/appleSlide)
+[无缝轮播](https://github.com/codevvvv9/goodSlide)
+[双向的轮播](https://github.com/codevvvv9/slides-demo1)
+2. 一个移动端PC端都能用的画板
+[画板](https://github.com/codevvvv9/canvas-demo)
+3. 一个静态简历
+4. 一个动态的可编辑的键盘导航栏
+`
+var result3 = `
+/*
+ *这就是我用原生js做的会动的简历
+ * 谢谢观看 ^_^
+ * /
+`
+writeCode('', result, () => { //write去calla这个function
+    //执行完了，才去createPaper
+    createPaper(() => {
+        writeCode(result, result2, () => {
+            writeMarkdown(md, () => {
+                writeCode(result, result2, () => {
+                    convertMarkdownToHtml(() => {
+                        writeCode(result+result2, result3, () => {
+                            console.log('写完了')
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
+
+/* 
+    异步的setInterval
+    1. 定闹钟
+    2. writeCode返回
+    3. 执行fn2()
+    4. 闹钟时间到
+    5. 写第一行代码
+
+    就好像 你先定了第二天6点闹钟，然后玩游戏。肯定是先玩游戏
+*/
+
 /**
  * 把code写到 #code和style标签里面
  * @param {*前缀} prefix 
@@ -70,45 +143,50 @@ function writeCode(prefix, code, fn) {
         domCode.scrollTop = domCode.scrollHeight//保证代码一定会看得到
         if (n >= code.length) {
            window.clearInterval(id)
-           fn.call()
+           fn && fn.call()
         }
     }, 10)
 }
 
-writeCode('', result, () => { //write去calla这个function
-    createPaper(() => {
-        writeCode(result, result2)
-    })
-})
-
-/* 
-    异步的setInterval
-    1. 定闹钟
-    2. writeCode返回
-    3. 执行fn2()
-    4. 闹钟时间到
-    5. 写第一行代码
-
-    就好像 你先定了第二天6点闹钟，然后玩游戏。肯定是先玩游戏
-*/
-
+/**
+ * 建立详细介绍页的函数
+ * @param {*函数} fn 
+ */
 function createPaper(fn) {
     var paper = document.createElement('div')
     paper.id = 'paper'
+    var content = document.createElement('pre')
+    content.className = 'content'
+    paper.appendChild(content)
     document.body.appendChild(paper)
-    fn.call()
+    fn && fn.call()
 }
 
-function fn3(preResult) {
-   
-    var n = 0
-    var id = setInterval(() => {
+/**
+ * 写markdown语法的函数
+ * @param {*markdown的内容} markdown 
+ * @param {*} fn 
+ */
+function writeMarkdown(markdown, fn) {
+    let domPaper = document.querySelector('#paper>.content')
+    let n = 0
+    let id = setInterval(() => {
         n += 1
-        code.innerHTML = preResult + result.substring(0, n)
-        code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css)
-        styleTag.innerHTML = preResult + result.substring(0, n)
-        if (n >= result.length) {
-            window.clearInterval(id)
+        domPaper.innerHTML = markdown.substring(0, n)
+        domPaper.scrollTop = domPaper.scrollHeight//保证代码一定会看得到
+        if (n >= markdown.length) {
+           window.clearInterval(id)
+           fn && fn.call()
         }
-    }, 50)
+    }, 10)
+
+}
+
+function convertMarkdownToHtml(fn){
+  var div = document.createElement('div')  
+  div.className = 'html markdown-body'
+  div.innerHTML = marked(md)
+  let markdownContainer = document.querySelector('#paper > .content')
+  markdownContainer.replaceWith(div)
+  fn && fn.call()
 }
